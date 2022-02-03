433fb47273a5457790af7a51f9f0e0a7308c89cd
Kunsanba <Kunsanba@gmail.com>
2022-02-03 18:36:57 +0300

    add google maps fragment

--- app/src/main/java/com/example/pillnotes/presentation/HomeFragment.kt
+++ app/src/main/java/com/example/pillnotes/presentation/HomeFragment.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.pillnotes.DaggerApplication
+import com.example.pillnotes.R
 import com.example.pillnotes.databinding.HomeFragmentBinding
 import com.example.pillnotes.domain.model.NoteTask
 import com.example.pillnotes.presentation.recycler.PillNotesAdapter
@@ -18,10 +20,11 @@ class HomeFragment : Fragment() {
     }
 
     private val items = listOf(
-        NoteTask("1", "1", "1","Ok", true, 1),
-        NoteTask("12", "1", "1","Ok", true, 2),
-        NoteTask("13", "1", "1","mnO", false, 3),
-        NoteTask("14", "1", "1",null, false, 1)
+        NoteTask("1", "1", "1", "Ok", true, 1),
+        NoteTask("12", "1", "1", "Ok", true, 2),
+        NoteTask("13", "1", "1", "mnO", false, 3),
+        NoteTask("14", "1", "1", null, false, 1),
+        NoteTask("1114", "1", "1", null, false, 1)
     )
 
     private val adapter by lazy { PillNotesAdapter(listOf()) }
@@ -38,10 +41,13 @@ class HomeFragment : Fragment() {
     override fun onStart() {
         super.onStart()
         initRecycler()
-
         adapter.updateList(
             items
         )
+
+        binding.button2.setOnClickListener {
+            findNavController().navigate(R.id.action_calendarFragment_to_mapsFragment)
+        }
     }
 
     private fun initRecycler() {
